7af29c522c95be5adddfee9a69c39c31322d8949
Akn <sibata486@gmail.com>
2023-06-06 17:33:42 +0900

    add /

--- TextSagasuKun/Model/SearchResult.cs
+++ TextSagasuKun/Model/SearchResult.cs
@@ -1,9 +1,28 @@
 using System.Collections.Generic;
+using System.Linq;
+using Prism.Mvvm;
 
 namespace TextSagasuKun.Model
 {
-    public class SearchResult
+    public class SearchResult : BindableBase
     {
-        public List<string> Results { get; set; } = new List<string>();
+        private string pattern = string.Empty;
+        private List<string> results = new List<string>();
+
+        public string Pattern { get => pattern; set => SetProperty(ref pattern, value); }
+
+        public List<string> Results
+        {
+            get => results;
+            set
+            {
+                if (SetProperty(ref results, value))
+                {
+                    RaisePropertyChanged(nameof(FirstResult));
+                }
+            }
+        }
+
+        public string FirstResult => Results.FirstOrDefault() != null ? Results.First() : string.Empty;
     }
 }
--- TextSagasuKun/ViewModels/MainWindowViewModel.cs
+++ TextSagasuKun/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,19 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using TextSagasuKun.Model;
 
 namespace TextSagasuKun.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
+
+        public TextStore TextStore { get; } = new TextStore();
+
+        public ObservableCollection<SearchResult> SearchResults { get; set; } = new ObservableCollection<SearchResult>()
+        {
+            new SearchResult(),
+        };
 
         public string Title
         {
