93106fbc0070e660e56e842628315aa786369f50
Akn <sibata486@gmail.com>
2023-06-06 16:58:49 +0900

    init /

---
+++ TextSagasuKun/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace TextSagasuKun.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
